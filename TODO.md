eca597b8c8965214a93192b7f8441e8d6c6b6758
cmetee-b <claudinebitee@gmail.com>
2025-03-16 00:20:32 +0100

    update algo

---
+++ TODO.md
@@ -0,0 +1,5 @@
+## TODO
+
+- [ ] add valgrind command line in Makefile (with possibility to pass args"
+	tips : 
+	$ ARGS="1 3 g" && valgrind ...
--- sources/push_swap/algo.c
+++ sources/push_swap/algo.c
@@ -6,30 +6,30 @@
 /*   By: asiatik <asiatik@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 19:23:11 by cmetee-b          #+#    #+#             */
-/*   Updated: 2025/03/15 23:15:59 by asiatik          ###   ########.fr       */
+/*   Updated: 2025/03/16 00:09:26 by asiatik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-// static int	is_sign_only_arg(char *arg)
-// {
-// 	return ((arg[0] == '+' || arg[0] == '-') && arg[1] == '\0');
-// }
+static int	is_sign_only_arg(char *arg)
+{
+	return ((arg[0] == '+' || arg[0] == '-') && arg[1] == '\0');
+}
 
-// static int	check_isolated_signs(int ac, char **av)
-// {
-// 	int	i;
+static int	check_isolated_signs(int ac, char **av)
+{
+	int	i;
 
-// 	i = 1;
-// 	while (i < ac)
-// 	{
-// 		if (is_sign_only_arg(av[i]))
-// 			return (1);
-// 		i++;
-// 	}
-// 	return (0);
-// }
+	i = 1;
+	while (i < ac)
+	{
+		if (is_sign_only_arg(av[i]))
+			return (1);
+		i++;
+	}
+	return (0);
+}
 
 t_data	*ft_algo_first_parse(char *arg)
 {
@@ -85,7 +85,16 @@ t_data	*ft_algo_parse(int ac, char **av)
 
 t_data	*parse_args(int ac, char **av)
 {
+	t_data	*a;
+
+	if (check_isolated_signs(ac, av))
+	{
+		ft_errors();
+		free_stack(&a);
+	}
 	if (ac == 2)
-		return (ft_algo_first_parse(av[1]));
-	return (ft_algo_parse(ac, av));
+		a = ft_algo_first_parse(av[1]);
+	else
+		a = ft_algo_parse(ac, av);
+	return (a);
 }
